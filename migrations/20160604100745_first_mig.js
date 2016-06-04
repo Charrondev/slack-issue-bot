1950e178961d4dc5865c842a68aca3be254fe0ad
Frederic Daigle <Freddydaigle@gmail.com>
2016-06-04 11:14:27 -0400

    stuff

--- migrations/20160604100745_first_mig.js
+++ migrations/20160604100745_first_mig.js
@@ -1,8 +1,31 @@
 
 exports.up = function(knex, Promise) {
-    
+    return Promise.all([
+        knex.schema.createTable('issue', function(table) {
+            table.increments('i_id').primary();
+            table.string('title');
+            table.string('author');
+            table.string('url');
+            table.string('issue_num');
+            table.text('text');
+            table.json('followers');
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+            table.timestamp('updated_at').defaultTo(knex.fn.now());
+        }),
+        knex.schema.createTable('comment', function(table) {
+            table.increments('c_id').primary();
+            table.string('i_id');
+            table.string('u_id');
+            table.text('text');
+            table.timestamp('posted_at').defaultTo(knex.fn.now());
+        })
+    ]);
+
 };
 
 exports.down = function(knex, Promise) {
-
+    return Promise.all([
+        knex.schema.dropTable('issue'),
+        knex.schema.dropTable('comment')
+    ]);
 };
