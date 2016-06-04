68daa24cfcfdeb6ac220f4ab090411ac99dd395c
charrondev <charrondev@gmail.com>
2016-06-04 11:12:48 -0400

    fa

--- bot/index.js
+++ bot/index.js
@@ -1,1 +1,14 @@
-const express = require('express');
+const Botkit = require('botkit');
+const path = require('path');
+
+const controller = Botkit.slackbot({
+  debug:true
+});
+
+controller.spawn({
+  token: require('../tokens.js').slack
+}).startRTM();
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+  bot.reply('hello');
+});
