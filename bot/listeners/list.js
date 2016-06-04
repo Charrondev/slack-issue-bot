5f7a7c066ed91847938fb3e80455cef55f271c9d
charrondev <charrondev@gmail.com>
2016-06-04 15:32:50 -0400

    Process list command

--- bot/listeners/list.js
+++ bot/listeners/list.js
@@ -1,11 +1,50 @@
+const knex = require('../../db');
+const _ = require('lodash');
+
 module.exports = (controller, bot) => {
   controller.hears(['list'], ['direct_message', 'direct_mention'], (bot, message) => {
-    // Check if closed is specified
+    if (!message.text.startsWith('list')) {
+      return;
+    };
 
-    // Check for labels
+    const commandProps = processProps(message.text);
 
-    // check for labels
-
-    // check
   });
+}
+
+function processProps(input) {
+  const options = {
+    showClosed: false,
+    showOpen: true,
+    labels: [],
+    contains: ""
+  };
+
+  const commands = input
+                    .replace('list', '')
+                    .trim()
+                    .split('-');
+  // deal with open / closed
+  commands.forEach(item => {
+    if (item.startsWith('closed')) options.showClosed = true;
+    if (item.startsWith('open')) options.showOpen = true;
+    if (item.startsWith('labels')) {
+       options.labels = item.replace('labels', '')
+                        .trim()
+                        .split(',');
+    }
+    if(item.startsWith('contains')) {
+      options.contains = item.replace('contains', '').trim();
+    }
+  });
+
+  return options;
+}
+
+function checkDatabase(options) {
+  // return knex('issue').
+}
+
+function createPost(options) {
+
 }
---
+++ bot/listeners/view.js
@@ -0,0 +1,11 @@
+module.exports = (controller, bot) => {
+  controller.hears(['view'], ['direct_message', 'direct_mention'], (bot, message) => {
+    // check issue number
+
+    // post
+
+    // Start conversation
+
+    //
+  });
+}
