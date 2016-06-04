ab7511e644ba3a67d3e9b6b6372d514aef0c79cf
charrondev <charrondev@gmail.com>
2016-06-04 13:07:06 -0400

    Save users to table

--- bot/setup.js
+++ bot/setup.js
@@ -1,6 +1,22 @@
+const knex = require('../db');
+
 module.exports = (controller, bot) => {
   // get all the users and add the to the database.
   bot.api.users.list({}, (err, response) => {
-    console.log(response);
+    knex('users')
+      .insert([sanitizeUsers])
+      .catch(error => {
+        // handle error
+      })
   });
 };
+
+function sanitizeUsers(users) {
+  return users
+          .filter(element => !element.is_bot)
+          .map(element => ({
+            id: element.id,
+            username: element.name,
+            real_name: element.real_name
+          }));
+}
