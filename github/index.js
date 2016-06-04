2c0e72cee74fb53f260fcb42a25d2b479d8bd502
Frederic Daigle <Freddydaigle@gmail.com>
2016-06-04 19:51:50 -0400

    working on getting more pages of issues

--- github/index.js
+++ github/index.js
@@ -1,4 +1,6 @@
-const github = require('./github_utils');
+const gitUtil = require('./github_utils');
 const knex = require('../db');
 
-github.format_timestamp()
+var sample_time = '2016-06-04T20:38:01Z';
+
+gitUtil.fetchAllIssues('nodejs','node');
